d29d78ba71b33758d7f831eec5c11f1e8b0348fe
David Montenegro <davidmontenegronet@gmail.com>
2024-09-06 18:57:07 -0300

    Merge branch 'main' of https://github.com/David-es-Montenegro/PortalMulher-CodeCrafters into Teste-Mapa

--- portal/src/App.js
+++ portal/src/App.js
@@ -1,10 +1,8 @@
 // src/App.js
 import React from 'react';
-import "./App.css"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Mapa from './Pages/Mapa/Mapa';
-
 
 function App() {
   return (
@@ -13,8 +11,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/mapa" element={<Mapa />} />
-
-       
         </Routes>
       </div>
     </Router>
